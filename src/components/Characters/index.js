3ffc88d3e01b6cb2de459e1cb9709b0f61e79523
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2022-08-24 12:29:34 +0530

    feat: Adds comments for characters, episodes, locations, nav side bar and app

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Characters.module.scss";
+/* Could organize imports, see App.js comments */
 
+/* Arrow function could be used here */
 function CharacterDetails({ character }) {
   return (
     <Link to={`${character.id}`}>
@@ -24,6 +26,13 @@ function CharacterDetails({ character }) {
 }
 
 const Characters = () => {
+  /*
+    You could move the state management and fetching part into a hook.
+    Lets call that hook useCharacters.
+
+    You can then use it like this:
+    const characters = useCharacters()
+  */
   const [characters, setCharacters] = useState();
 
   useEffect(() => {
